23e6e26db784b5fba1fd73ca1f050450f26e409c
Chris James <chris.james@bbc.com>
2016-02-23 14:16:47 +0000

    arrow function

--- src/store/create-store.js
+++ src/store/create-store.js
@@ -3,7 +3,7 @@ import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
-export default function configureStore (initialState) {
+export default (initialState) => {
   const logger = createLogger({
     collapsed: true
   });
